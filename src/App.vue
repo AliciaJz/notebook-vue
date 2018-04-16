bfa1a2d1ac589cb00fce5490caf6a3730655c954
AliciaJz <zilaphono@gmail.com>
2018-04-16 15:30:01 -0500

    antes de firebase

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,60 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <!-- <img src="./assets/logo.png"> -->
+    <!-- <router-view/> -->
+    <Notebook @change-page="changePage" @new-page="newPage" :pages="pages" :activePage="index" />
+    <Page @save-page="savePage" @delete-page="deletePage" :page="pages[index]" />
   </div>
 </template>
 
 <script>
+import Notebook from './components/Notebook'
+import Page from './components/Page'
+
 export default {
-  name: 'App'
-}
+      name: 'app',
+      components: {
+        Notebook,
+        Page
+      },
+      data: () => ({
+        pages: [],
+        index: 0
+      }),
+      methods: {
+        newPage () {
+          this.pages.push({
+            title: '',
+            content: ''
+          })
+          this.index = this.pages.length - 1
+        },
+        changePage (index) {
+          this.index = index
+        },
+        savePage () {
+          // nothing as of yet
+        },
+        deletePage () {
+          this.pages.splice(this.index, 1)
+          this.index = Math.max(this.index - 1, 0)
+        }
+      }
+    }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+    html, body, #app {
+        height: 100%;
+    }
+
+    body {
+        margin: 0;
+    }
+
+    #app {
+        font-family: 'Avenir', Helvetica, Arial, sans-serif;
+        display: flex;
+        flex-direction: row;
+    }
+    </style>
